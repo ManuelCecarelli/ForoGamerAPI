7ccc9e237fb888a6120f764f7a5e1578e5e841c6
ManuelCecarelli <manuelcecarelli@gmail.com>
2025-04-11 16:12:00 -0300

    I create some custom exceptions

---
+++ src/Domain/Exceptions/AppValidationException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Exceptions
+{
+    //Exception thrown by Application layer. Mostly bussiness rules validations or data type format validation.
+    public class AppValidationException : Exception
+    {
+        public AppValidationException() : base()
+        {
+        }
+
+        public AppValidationException(string message) : base(message)
+        {
+        }
+
+        public AppValidationException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ src/Domain/Exceptions/NotAllowedException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Exceptions
+{
+    public class NotAllowedException : Exception
+    {
+        /* Custom exception to throw when a user attempts to perform an
+         * action that is not allowed or for which they do not have permissions */
+
+        public NotAllowedException() : base()
+        {
+        }
+
+        public NotAllowedException(string message) : base(message)
+        {
+        }
+
+        public NotAllowedException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
