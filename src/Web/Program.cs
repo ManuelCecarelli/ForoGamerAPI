cb44da8ec7e956ea71af6240a8de18b601efb865
ManuelCecarelli <manuelcecarelli@gmail.com>
2025-04-15 13:40:21 -0300

    Program.cs reordering

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -8,9 +8,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+#region SwaggerConfig
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
+
+#endregion
+
+#region DataBaseConfig
 
 // DB connection String
 string connectionString = builder.Configuration["ConnectionStrings:ForoGamerApiDbDefaultConnection"]!;
@@ -27,6 +34,20 @@ using (var command = connection.CreateCommand())
 }
 
 builder.Services.AddDbContext<ApplicationDbContext>(dbContextOptions => dbContextOptions.UseSqlite(connectionString));
+
+#endregion
+
+#region Services
+
+
+
+#endregion
+
+#region Repositories
+
+
+
+#endregion
 
 var app = builder.Build();
 
