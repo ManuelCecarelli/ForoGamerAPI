07dc11ec86f3893ba7d0ea229b11d64bd9e0e685
ManuelCecarelli <manuelcecarelli@gmail.com>
2025-05-05 12:05:03 -0300

    Create GenreConfiguration

--- src/Infrastructure/Data/ApplicationDbContext.cs
+++ src/Infrastructure/Data/ApplicationDbContext.cs
@@ -15,57 +15,13 @@ namespace Infrastructure.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Genre>()
-                .Property(g => g.Id)
-                .ValueGeneratedOnAdd();
-
             modelBuilder.Entity<Platform>()
                 .Property(p => p.Id)
                 .ValueGeneratedOnAdd();
 
-            modelBuilder.Entity<Genre>().HasData(CreateGenreDataSeed());
             modelBuilder.Entity<Platform>().HasData(CreatePlatformDataSeed());
 
             base.OnModelCreating(modelBuilder);
-        }
-
-        private Genre[] CreateGenreDataSeed()
-        {
-            Genre[] newGenres = [
-
-                new Genre()
-                {
-                    Id = 1,
-                    Name = "None",
-                    Description = "Sin género"
-                },
-                new Genre()
-                {
-                    Id = 2,
-                    Name = "RTS",
-                    Description = "Real Time Strategy"
-                },
-                new Genre
-                {
-                    Id = 3,
-                    Name = "Sports",
-                    Description = null
-                },
-                new Genre
-                {
-                    Id = 4,
-                    Name = "Fighting",
-                    Description = null
-                },
-                new Genre
-                {
-                    Id = 5,
-                    Name = "Racing",
-                    Description = null
-                },
-            ];
-
-            return newGenres;
         }
 
         private Platform[] CreatePlatformDataSeed()
---
+++ src/Infrastructure/Data/Configurations/GenreConfiguration.cs
@@ -0,0 +1,59 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Configurations
+{
+    public class GenreConfiguration : IEntityTypeConfiguration<Genre>
+    {
+        public void Configure(EntityTypeBuilder<Genre> builder)
+        {
+            builder.Property(g => g.Name).IsRequired().HasMaxLength(30);
+            builder.HasData(CreateGenreDataSeed());
+        }
+
+        private Genre[] CreateGenreDataSeed()
+        {
+            Genre[] newGenres = [
+
+                new Genre()
+                {
+                    Id = 1,
+                    Name = "None",
+                    Description = "Sin género"
+                },
+                new Genre()
+                {
+                    Id = 2,
+                    Name = "RTS",
+                    Description = "Real Time Strategy"
+                },
+                new Genre
+                {
+                    Id = 3,
+                    Name = "Sports",
+                    Description = null
+                },
+                new Genre
+                {
+                    Id = 4,
+                    Name = "Fighting",
+                    Description = null
+                },
+                new Genre
+                {
+                    Id = 5,
+                    Name = "Racing",
+                    Description = null
+                },
+            ];
+
+            return newGenres;
+        }
+    }
+}
